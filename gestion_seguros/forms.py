15466e8390213c815ae939503d057a4484f36d2d
Max Abdeneve <maxabdeneve@gmail.com>
2023-05-05 18:57:09 -0300

    Cambio label "Cotnraseña (*)" -> "Contraseña (*)"

--- gestion_seguros/forms.py
+++ gestion_seguros/forms.py
@@ -7,13 +7,13 @@ TYPE_CHOICES={
 }
 
 class AltaClienteForm(forms.Form):
-    razonSocial = forms.CharField(max_length=30, min_length=3, required=True, 
-        widget=forms.TextInput(), 
+    razonSocial = forms.CharField(max_length=30, min_length=3, required=True,
+        widget=forms.TextInput(),
         label="Razón Social (*)")
     CUIT = forms.CharField(max_length=30, min_length=3, required=True, label="CUIT (*)")
     email = forms.EmailField(label="Email (*)", required=True)
     usuario = forms.CharField(max_length=20, min_length=4, required=True, label="Usuario (*)")
-    contrasena = forms.CharField(max_length=20, min_length=5, widget=forms.PasswordInput, required=True, label="Cotnraseña (*)")
+    contrasena = forms.CharField(max_length=20, min_length=5, widget=forms.PasswordInput, required=True, label="Contraseña (*)")
     domicilio = forms.CharField(max_length=60, required=False, label="Domicilio")
     telefono = forms.IntegerField(required=False, label="Telefono")
 
