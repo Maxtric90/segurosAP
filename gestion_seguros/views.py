633c6e2a1209e0e0eaccec11206fbfeac9a79c55
Maximiliano Ortiz (San Fernando) <m.max.x@hotmail.com>
2023-05-14 22:22:35 -0300

    commit message

---
+++ gestion_seguros/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import AltaClienteForm
+from .models import Cliente
+
+# Create your views here.
+def index(request):
+    context={}
+    return render(request, 'gestion_seguros/index.html', context)
+
+def gestion_clientes(request):
+    clientes=Cliente.objects.all()
+
+    if request.method == "POST":
+        alta_cliente_form = AltaClienteForm(request.POST)
+        if alta_cliente_form.is_valid():
+            print(alta_cliente_form.cleaned_data['razonSocial'])
+            return redirect("index")
+    else:
+        alta_cliente_form = AltaClienteForm()
+    context = {'clientes': clientes, 'form': alta_cliente_form}
+    print(request.POST)
+    return render(request, 'gestion_seguros/gestion_clientes.html', context)
+
+def gestion_polizas(request):
+    context={}
+    return render(request, 'gestion_seguros/gestion_polizas.html', context)
+
+def gestion_asegurados(request):
+    context={}
+    return render(request, 'gestion_seguros/gestion_asegurados.html', context)
